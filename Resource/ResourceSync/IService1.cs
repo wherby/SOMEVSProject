0bae9192dee73cd9241fa95d8d3f1ffe77eb3a79
wherby <187225577@qq.com>
2013-08-09 14:44:58 +0800

    add log function

--- Resource/ResourceSync/IService1.cs
+++ Resource/ResourceSync/IService1.cs
@@ -21,36 +21,39 @@ namespace ResourceSync
         void ResetAllResource();
 
         [OperationContract]
-        int Add(int x, int y);
+        void SetAllDebugTrace();
 
-        [OperationContract]
-        string GetData(int value);
+        //[OperationContract]
+        //int Add(int x, int y);
 
-        [OperationContract]
-        CompositeType GetDataUsingDataContract(CompositeType composite);
+        //[OperationContract]
+        //string GetData(int value);
+
+        //[OperationContract]
+        //CompositeType GetDataUsingDataContract(CompositeType composite);
 
         // TODO: Add your service operations here
     }
 
-    // Use a data contract as illustrated in the sample below to add composite types to service operations
-    [DataContract]
-    public class CompositeType
-    {
-        bool boolValue = true;
-        string stringValue = "Hello ";
+    //// Use a data contract as illustrated in the sample below to add composite types to service operations
+    //[DataContract]
+    //public class CompositeType
+    //{
+    //    bool boolValue = true;
+    //    string stringValue = "Hello ";
 
-        [DataMember]
-        public bool BoolValue
-        {
-            get { return boolValue; }
-            set { boolValue = value; }
-        }
+    //    [DataMember]
+    //    public bool BoolValue
+    //    {
+    //        get { return boolValue; }
+    //        set { boolValue = value; }
+    //    }
 
-        [DataMember]
-        public string StringValue
-        {
-            get { return stringValue; }
-            set { stringValue = value; }
-        }
-    }
+    //    [DataMember]
+    //    public string StringValue
+    //    {
+    //        get { return stringValue; }
+    //        set { stringValue = value; }
+    //    }
+    //}
 }
--- Resource/ResourceSync/Service1.cs
+++ Resource/ResourceSync/Service1.cs
@@ -11,11 +11,16 @@ namespace ResourceSync
     public class Service1 : IResource
     {
         static SortedList<string, bool> resourcePool = new SortedList<string, bool>();
+        TestLog log;
+        static bool DEBUGTRACE = true;
+
         private object threadLock = new object();
 
         public bool GetResource(string resourceID)
         {
             bool resourceAvailability=false;
+
+           
             lock (threadLock)
             {
                 if (resourcePool.Keys.Contains(resourceID))
@@ -24,16 +29,31 @@ namespace ResourceSync
                     {
                         resourcePool[resourceID] = false;
                         resourceAvailability = true;
+                        if (DEBUGTRACE)
+                        {
+                            log = TestLog.GetInstance();
+                            log.LogInfo(string.Format("{0} is Allocated", resourceID));
+                        }
                     }
                     else
                     {
                         resourceAvailability = false;
+                        if (DEBUGTRACE)
+                        {
+                            log = TestLog.GetInstance();
+                            log.LogInfo(string.Format("{0} had been used, pending", resourceID));
+                        }
                     }
                 }
                 else
                 {
                     resourcePool.Add(resourceID, false);
                     resourceAvailability = true;
+                    if (DEBUGTRACE)
+                    {
+                        log = TestLog.GetInstance();
+                        log.LogInfo(string.Format("{0} is Allocated", resourceID));
+                    }
                 }
             }
             return resourceAvailability;
@@ -46,6 +66,11 @@ namespace ResourceSync
                 if (resourcePool.Keys.Contains(resourceID))
                 {
                     resourcePool[resourceID] = true;
+                    if (DEBUGTRACE)
+                    {
+                        log = TestLog.GetInstance();
+                        log.LogInfo(string.Format("{0} is Released", resourceID));
+                    }
                 }
             }
         }
@@ -58,28 +83,40 @@ namespace ResourceSync
             }
         }
 
-        public int Add(int x, int y)
-        {
-            System.Threading.Thread.Sleep(1000);
-            return x + y;
-        }
 
-        public string GetData(int value)
+        public  void SetAllDebugTrace()
         {
-            return string.Format("You entered: {0}", value);
+            if (DEBUGTRACE == true)
+            {
+                DEBUGTRACE = false;
+            }
+            else
+            {
+                DEBUGTRACE = true;
+            }
         }
+        //public int Add(int x, int y)
+        //{
+        //    System.Threading.Thread.Sleep(1000);
+        //    return x + y;
+        //}
 
-        public CompositeType GetDataUsingDataContract(CompositeType composite)
-        {
-            if (composite == null)
-            {
-                throw new ArgumentNullException("composite");
-            }
-            if (composite.BoolValue)
-            {
-                composite.StringValue += "Suffix";
-            }
-            return composite;
-        }
+        //public string GetData(int value)
+        //{
+        //    return string.Format("You entered: {0}", value);
+        //}
+
+        //public CompositeType GetDataUsingDataContract(CompositeType composite)
+        //{
+        //    if (composite == null)
+        //    {
+        //        throw new ArgumentNullException("composite");
+        //    }
+        //    if (composite.BoolValue)
+        //    {
+        //        composite.StringValue += "Suffix";
+        //    }
+        //    return composite;
+        //}
     }
 }
