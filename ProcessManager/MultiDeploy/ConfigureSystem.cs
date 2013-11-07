8240c815d722dc5b7beac19035589ef8fd9dc5f5
wherby <187225577@qq.com>
2013-11-07 18:19:19 +0800

    refine process manager to have script output

---
+++ ProcessManager/MultiDeploy/ConfigureSystem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ProcessManager
+{
+    public partial class ConfigureSystem : Form
+    {
+        public ConfigureSystem()
+        {
+            InitializeComponent();
+        }
+
+        private void EditSystem_Click(object sender, EventArgs e)
+        {
+            GlobalData.systemConfigString = EditSystemConfig.Text;
+
+        }
+
+        private void ConfigureSystem_Load(object sender, EventArgs e)
+        {
+            EditSystemConfig.Text = GlobalData.systemConfigString;
+        }
+    }
+}
---
+++ ProcessManager/MultiDeploy/PostConfig.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ProcessManager
+{
+    public partial class PostConfig : Form
+    {
+        public PostConfig()
+        {
+            InitializeComponent();
+        }
+
+        private void PostConfig_Load(object sender, EventArgs e)
+        {
+            foreach (string item in GlobalData.postfixFileString)
+            {
+                PostConfigFile.Items.Add(item);
+            }
+        }
+
+        private void Addpost_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialogPost.ShowDialog();
+            string folder = folderBrowserDialogPost.SelectedPath;
+            PostConfigFolder.Items.Add(folder);
+        }
+
+        private void RemovePost_Click(object sender, EventArgs e)
+        {
+            int index = PostConfigFolder.SelectedIndex;
+            PostConfigFolder.Items.RemoveAt(index);
+        }
+
+        private void PostConfigFolder_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            GlobalData.postfixFileString.Clear();
+            for (int i = 0; i < PostConfigFolder.Items.Count; i++)
+            {
+                string[] tempFiles = Helper.GetAllFiles(PostConfigFolder.Items[i].ToString());
+                GlobalData.postfixFileString.AddRange(tempFiles);
+            }
+
+            PostConfigFile.Items.Clear();
+            foreach (string temp in GlobalData.postfixFileString)
+            {
+                PostConfigFile.Items.Add(temp);
+            }
+        }
+
+    }
+}
