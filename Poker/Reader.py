8c8a5fdb9be73f839d31c5864e3fc1e1c522ecaa
wherby <187225577@qq.com>
2014-06-19 13:30:51 +0800

    add the status

--- Poker/Reader.py
+++ Poker/Reader.py
@@ -37,14 +37,16 @@ class ReadFile:
         return self.hands
     
     def __str__(self):
-        return self.hands[1];
+        return self.hands[8];
 
 
 class Hand:
     def __init__(self,lines):
         self.lines=lines.splitlines(True);
         self.heros=[]
-        print lines;
+        #print lines;
+        self.states=[]
+        self.summary=[]
 
     def getNumber(self):
         m = re.match(r'(.*)(Hand #)(\d*)(:)(.*)', self.lines[0])
@@ -60,6 +62,36 @@ class Hand:
                     self.heros.append(m.group(2))         
             pass
 
+    def getStates(self):
+        temp="";
+        start=0;
+        num=0;
+        for line in self.lines:
+            num=num+1;
+            if start==1:
+                if line.find("***")<0:
+                    if temp=="":
+                        temp=line.strip();
+                    else:
+                        temp=temp+line;
+                else:
+                    self.states.append(temp);
+                    temp="";
+            if start==2:
+                if temp=="":
+                    temp=line.strip();
+                else:
+                    temp=temp+line;
+                if num==len(self.lines):
+                    self.summary.append(temp);
+                    temp="";     
+            if line.find("*** HOLE CARDS ***")>=0:
+                start=1;
+            if line.find("*** SUMMARY ***")>=0:
+                start=2;
+            
+            
+
     def __str__(self):
         temp="Heros :\n"
         for hero in self.heros:
@@ -70,11 +102,14 @@ if __name__=="__main__":
     temp=ReadFile(".\HandHistory\scuipio\HH20121012 Halley - $0.01-$0.02 - USD No Limit Hold'em.txt")
     temp.readAllLine();
     temp.splitToHand();
-    print temp
+    #print temp
     hands=temp.getHands();
-    print hands[1];
-    a=Hand(hands[1])
+    print hands[7];
+    a=Hand(hands[7])
     a.getNumber();
     a.getHeros();
     print a;
+    a.getStates();
+    #print "states number of a: "+str(len(a.states))
+    #print "summary of a: "+a.summary[0]
     
