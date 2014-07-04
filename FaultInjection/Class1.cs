721bcda567fd561e0da26c20eacf6e53e6baf5b9
wherby <187225577@qq.com>
2014-07-04 16:06:23 +0800

    fault injection

---
+++ FaultInjection/Class1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FaultInjection
+{
+    public static class FaultInjectionAdapter
+    {
+        static public void SetFaultCode<T>(T d, string fileName)
+        {
+            string[] alllines = File.ReadAllLines(fileName);
+
+
+            #region
+
+            foreach (string temp in alllines)
+            {
+                string[] temp2 = temp.Trim().Split(new string[] { "=" }, StringSplitOptions.RemoveEmptyEntries);
+                if (temp2.Count<string>() == 2)
+                {
+                    try
+                    {
+                        foreach (PropertyInfo temp3 in d.GetType().GetProperties())
+                        {
+                            if (temp3.ToString().Contains(temp2[0]))
+                            {
+                                if (d.GetType().GetProperty(temp2[0]).PropertyType == "a".GetType())
+                                {
+                                    d.GetType().GetProperty(temp2[0]).SetValue(d, temp2[1]);
+                                }
+                                else if (d.GetType().GetProperty(temp2[0]).PropertyType.BaseType.FullName == "System.Enum")
+                                {
+                                    var obEn = Enum.Parse(d.GetType().GetProperty(temp2[0]).PropertyType, temp2[1]);
+                                    d.GetType().GetProperty(temp2[0]).SetValue(d, obEn);
+                                }
+                                else if (d.GetType().GetProperty(temp2[0]).PropertyType.GetElementType().BaseType.FullName == "System.Enum")
+                                {
+                                    var obEn = Enum.Parse(d.GetType().GetProperty(temp2[0]).PropertyType.GetElementType(), temp2[1]);
+                                    var aEn = Array.CreateInstance(d.GetType().GetProperty(temp2[0]).PropertyType.GetElementType(), 1);
+                                    aEn.SetValue(obEn, 0);
+                                    d.GetType().GetProperty(temp2[0]).SetValue(d, aEn);
+                                }
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        string message = ex.Message;
+                    }
+                }
+                if (temp2.Count<string>() == 4)
+                {
+                    try
+                    {
+                        foreach (PropertyInfo temp3 in d.GetType().GetProperties())
+                        {
+                            if (temp3.ToString().Contains(temp2[0]))
+                            {
+                                if ((d.GetType().GetProperty(temp2[0]).GetValue(d)).ToString() == temp2[1])
+                                {
+                                    foreach (PropertyInfo temp4 in d.GetType().GetProperties())
+                                    {
+                                        if (temp4.ToString().Contains(temp2[2]))
+                                        {
+                                            if (d.GetType().GetProperty(temp2[2]).PropertyType == "a".GetType())
+                                            {
+                                                d.GetType().GetProperty(temp2[2]).SetValue(d, temp2[3]);
+                                            }
+                                            else if (d.GetType().GetProperty(temp2[2]).PropertyType.BaseType.FullName == "System.Enum")
+                                            {
+                                                var obEn = Enum.Parse(d.GetType().GetProperty(temp2[2]).PropertyType, temp2[3]);
+                                                d.GetType().GetProperty(temp2[2]).SetValue(d, obEn);
+                                            }
+                                            else if (d.GetType().GetProperty(temp2[2]).PropertyType.GetElementType().BaseType.FullName == "System.Enum")
+                                            {
+                                                var obEn = Enum.Parse(d.GetType().GetProperty(temp2[2]).PropertyType.GetElementType(), temp2[3]);
+                                                var aEn = Array.CreateInstance(d.GetType().GetProperty(temp2[2]).PropertyType.GetElementType(), 1);
+                                                aEn.SetValue(obEn, 0);
+                                                d.GetType().GetProperty(temp2[2]).SetValue(d, aEn);
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        string message = ex.Message;
+                    }
+                }
+            }
+
+            #endregion
+
+
+        }
+    }
+}
---
+++ FaultInjection/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("FaultInjection")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("FaultInjection")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ce2e7554-5613-4c0b-b6a6-a0292cae6121")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
